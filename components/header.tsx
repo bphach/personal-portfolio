cd192f927c91426c7a6efcd0e598207f4916c5ad
Bryan Hach <soturota@gmail.com>
2023-10-30 12:00:37 -0400

    initial commit

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { motion } from "framer-motion";
+import { links } from "@/lib/data";
 
 export default function Header() {
   return (
@@ -12,6 +13,9 @@ export default function Header() {
       animate={{ y: 0, x: "-50%", opacity: 1 }}
     ></motion.div> 
   
+    <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+    <ul></ul>
+    </nav>
   </header>
   );
 }
