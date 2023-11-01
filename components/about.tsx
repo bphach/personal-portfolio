fcad43ffeeeb087a99f05d28b55033a76be36cfc
bphach <soturota@gmail.com>
2023-11-01 17:12:38 -0400

    ss

---
+++ components/about.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function About() {
+  return (
+  <section>
+    <h2>About me</h2>
+    <p>
+    After graduating with a degree in{" "}
+        <span className="font-medium">Science</span>, I decided to pursue my
+        passion for creating. Currently enroll at university and learning{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Python, and AWS
+        </span>
+        . I am also familiar with TypeScrip. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+    </p>
+
+    <p>
+    <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and cooking delicious food. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">drawing and animating </span>. I'm also
+        learning how to play the guitar.
+    </p>
+  </section>
+  );
+}
